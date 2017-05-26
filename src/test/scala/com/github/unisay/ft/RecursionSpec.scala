8fdbf1f92a9b02485367c40fa89cbb8508f35314
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2017-05-26 11:45:42 +0200

    mutual recursion fixed

--- src/test/scala/com/github/unisay/ft/RecursionSpec.scala
+++ src/test/scala/com/github/unisay/ft/RecursionSpec.scala
@@ -1,22 +1,23 @@
 package com.github.unisay.ft
 
 import cats.Eval
+import cats.Eval.always
 import org.specs2._
 
 class RecursionSpec extends Specification { def is = s2"""
- stackOverflow              $stackOverflow
- noStackOverflow            $noStackOverflow
+ Stream[Int]           $testStream
+ Eval[Stream[Int]]     $testEvalStream
  """
 
-  def noStackOverflow = {
+  def testStream = {
     def one: Stream[Int] = 1 #:: zer // 1 #:: 0 #:: 1 #:: ⊥
     def zer: Stream[Int] = 0 #:: one // 0 #:: 1 #:: 0 #:: ⊥
     one.take(3).toList must_=== List(1, 0, 1)
   }
 
-  def stackOverflow = {
-    def one: Eval[Stream[Int]] = zer.map(0 #:: _)
-    def zer: Eval[Stream[Int]] = one.map(1 #:: _)
+  def testEvalStream = {
+    def one: Eval[Stream[Int]] = always(1 #:: zer.value)
+    def zer: Eval[Stream[Int]] = always(0 #:: one.value)
     one.map(_.take(3).toList).value must_=== List(1, 0, 1)
   }
 }
